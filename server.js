3e1af31afe4435ce2656768f322359b5b2f61b3d
Alexander Olivares <olivaresa@gmail.com>
2016-08-17 22:43:05 +0000

    agregando router

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-
-
 var http = require("http");
+var url = require("url");
 
-function iniciar() {
+function iniciar(route) {
   function onRequest(request, response) {
-    console.log("Petición Recibida.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Peticion para " + pathname + " recibida.");
+
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("Hola Mundo");
     response.end();
@@ -15,44 +18,3 @@ function iniciar() {
 }
 
 exports.iniciar = iniciar;
-
-/*
-var http = require("http");
-
-function onRequest(request, response) {
-  console.log("Peticion Recibida.");
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Mundo");
-  response.end();
-}
-
-http.createServer(onRequest).listen(8888);
-
-console.log("Servidor Iniciado.");
-*/
-
-/*
-var http = require("http");
-
-function onRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Mundo 2");
-  response.end();
-}
-
-http.createServer(onRequest).listen(8888);
-
-*/
-
-/*
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Mundo");
-  response.end();
-}).listen(8888);
-
-*/
-
-
