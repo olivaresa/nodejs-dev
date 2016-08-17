30ea95f72f52f13e33e828b6f88b73e86560c6c6
Alexander Olivares <olivaresa@gmail.com>
2016-08-17 23:35:23 +0000

    probando operaciones bloqueantes

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,14 @@
 function iniciar() {
   console.log("Manipulador de peticion 'iniciar' fue llamado.");
+
+  function sleep(milliSeconds) {  
+    // obten la hora actual
+    var startTime = new Date().getTime();
+    // atasca la cpu
+    while (new Date().getTime() < startTime + milliSeconds); 
+  }
+
+  sleep(10000);
   return "Hola Iniciar";
 }
 
