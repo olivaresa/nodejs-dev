dbe8b8d9312546bbf744c884d94a8d0af4615af9
Alexander Olivares <olivaresa@gmail.com>
2016-08-18 00:08:50 +0000

    agregando querystring

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,7 @@
+var querystring = require("querystring");
+
 function iniciar(response, postData) {
-  console.log("Manipulador de Peticion 'iniciar' fue llamado.");
+  console.log("Manipulador de peticion 'inicio' fue llamado.");
 
   var body = '<html>'+
     '<head>'+
@@ -8,21 +10,22 @@ function iniciar(response, postData) {
     '<body>'+
     '<form action="/subir" method="post">'+
     '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Enviar texto" />'+
+    '<input type="submit" value="Submit text" />'+
     '</form>'+
     '</body>'+
     '</html>';
-  
+
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
 }
 
 function subir(response, dataPosteada) {
-  console.log("Manipulador de Peticion 'subir' fue llamado.");
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Tu enviaste: " + dataPosteada);
-  response.end();
+    console.log("Manipulador de peticion 'subir' fue llamado.");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Tu enviaste el texto: : " +
+    querystring.parse(dataPosteada)["text"]);
+    response.end();
 }
 
 exports.iniciar = iniciar;
